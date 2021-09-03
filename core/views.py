e1eabeb065ad85914cd173fd5ba463a8222f7257
Joao Aragao <joaovictoraragaoads@gmail.com>
2021-09-02 22:53:20 -0300

    Fix templates not found

--- core/views.py
+++ core/views.py
@@ -4,12 +4,15 @@ from django.shortcuts import render
 
 
 def index(request):
+
     return render(request, 'index.html')
 
 
 def contato(request):
+
     return render(request, 'contato.html')
 
 
 def produto(request):
+
     return render(request, 'produto.html')
